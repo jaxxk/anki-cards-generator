04bd83c549e2178df5014d68a1951edbeabe0c4d
jaxxk <forwarwick@live.com>
2024-12-26 11:10:52 -0800

    Add Loggin, Edit Generate Description

--- cmd/generate.go
+++ cmd/generate.go
@@ -12,13 +12,12 @@ import (
 // generateCmd represents the generate command
 var generateCmd = &cobra.Command{
 	Use:   "generate",
-	Short: "A brief description of your command generate",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Generates anki flashcards based off input md/txt file",
+	Long: `generate expects the flag --file or -f followed by the full path of the
+	file. Example: 
+	
+	poggers generate -f /Users/jaxk/notes/notes.md or poggers generate --file ~/notes/notes.md
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("generate called")
 	},
--- internal/transform/transform.go
+++ internal/transform/transform.go
@@ -1,1 +1,5 @@
 package transform
+
+/*
+Transform package takes user supplied md/txt file, uses chatgpt to scan through the md/txt file to generate corresponding flashcards.
+*/
